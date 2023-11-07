3eab0a17915a48cdc3561f5005119b94cf038afc
scarlettsimon <146833816+scarlettsimon@users.noreply.github.com>
2023-11-07 08:07:45 +0100

    Basic implementation

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,28 @@
 import { supa } from "../config/config.js";
+
+const supabaseUrl = '';
+const supabasePublicKey = '';
+
+const supabase = supabase.createClient(supabaseUrl, supabasePublicKey);
+
+document.getElementById('login-form').addEventListener('submit', async (e) => {
+    e.preventDefault();
+
+    const email = document.getElementById('email').value;
+    const password = document.getElementById('password').value;
+
+    try {
+        const { user, session, error } = await supabase.auth.signIn({
+            email,
+            password,
+        });
+
+        if (error) {
+            alert(error.message);
+        } else {
+            alert('Login successful');
+        }
+    } catch (error) {
+        console.error('Error:', error.message);
+    }
+});
