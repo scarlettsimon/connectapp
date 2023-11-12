b976a54a5a6ed1b97a340d1ab593ea9504827ee1
scarlettsimon <146833816+scarlettsimon@users.noreply.github.com>
2023-11-12 11:03:05 +0100

    Update user information in the db

--- public/scripts/auth.js
+++ public/scripts/auth.js
@@ -2,14 +2,15 @@
 import { createClient } from 'https://cdn.skypack.dev/@supabase/supabase-js';
 const supabaseUrl = 'https://ooxiuecaotwshyucwybn.supabase.co';
 const supabasePublicKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9veGl1ZWNhb3R3c2h5dWN3eWJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE2OTYzMzY3MzMsImV4cCI6MjAxMTkxMjczM30.XHxAVR2xvZBj6rqtJEmbG7pzGiN_g6b62EVOtxEgndg';
+const supabase = createClient(supabaseUrl, supabasePublicKey);
 
 // LOGIN
 export async function handleLogin() {
-    const supabase = createClient(supabaseUrl, supabasePublicKey);
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     try {
+        // Logging in user
         const { data, error } = await supabase.auth.signInWithPassword({
             email,
             password,
@@ -35,13 +36,13 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // SIGNUP
 export async function handleSignUp() {
-    const supabase = createClient(supabaseUrl, supabasePublicKey);
     const name = document.getElementById('name').value;
     const surname = document.getElementById('surname').value;
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
     try {
+        // Creating a new user
         const { data, error } = await supabase.auth.signUp({
             email,
             password,
@@ -50,6 +51,7 @@ export async function handleSignUp() {
         if (error) {
             alert(error.message);
         } else {
+            // Inserting user data into the database
             const { data, error: insertError } = await supabase
                 .from('users')
                 .upsert([
@@ -81,21 +83,113 @@ document.addEventListener('DOMContentLoaded', () => {
 
 async function getUsers() {
     let { data, error } = await supabase
-        .from('users') // replace with your table name
-        .select('*') // list the columns you need
-        .eq('id', 'user_id') // use appropriate filtering
+        .from('users')
+        .select('*')
     return data;
 }
 
-async function getUser(user) {
+async function getUser() {
+    const { data: { user } } = await supabase.auth.getUser();
+    if (!user) {
+        return null;
+    }
     let { data, error } = await supabase
-        .from('users') 
-        .select('*') 
-        .eq('id', 'user_id')
-    return data;
+        .from('users')
+        .select('*')
+        .eq('email', user.email);
+
+    return data[0];
 }
 
-function populateForm(userData) {
-    document.getElementById('John Doe').value = userData.name;
-    document.getElementById('johndoe@gmail.com').value = userData.email;
+
+function createProfileCard(user) {
+    return `
+        <div class="profile-card">
+            <div class="profile-header">
+                <img src="${user.profile_image}" alt="${user.name}" style="height:50px;width:50px;object-fit: cover;">
+                <h2>${user.name}</h2>
+            </div>
+            <div class="profile-content">
+                <p>${user.description}</p>
+            </div>
+            <button class="view-profile-btn">VIEW PROFILE</button>
+        </div>
+    `;
 }
+
+async function populateProfileCards() {
+    const users = await getUsers();
+    if (users.length > 0) {
+        const cardsHtml = users.map(createProfileCard).join('');
+        document.getElementById('profile-cards-container').innerHTML = cardsHtml;
+        document.querySelector('.view-profile-btn').addEventListener('click', function () {
+            window.location.href = `https://www.instagram.com/${'asome'}/`;
+        });
+    }
+}
+
+document.addEventListener('DOMContentLoaded', populateProfileCards);
+
+document.addEventListener('DOMContentLoaded', async function () {
+    const nameInput = document.getElementById('name');
+    const surnameInput = document.getElementById('surname');
+    const emailInput = document.getElementById('email');
+    const descriptionInput = document.getElementById('description');
+    const instagramInput = document.getElementById('instagram');
+    const userData = await getUser();
+
+    if (userData) {
+        console.log(userData);
+        nameInput.value = userData.name || '';
+        surnameInput.value = userData.surname || '';
+        emailInput.value = userData.email || '';
+        descriptionInput.value = userData.description || '';
+        instagramInput.value = userData.instagram_account || '';
+    }
+});
+
+async function updateUserData () {
+    const nameInput = document.getElementById('name');
+    const surnameInput = document.getElementById('surname');
+    const emailInput = document.getElementById('email');
+    const descriptionInput = document.getElementById('description');
+    const instagramInput = document.getElementById('instagram');
+
+    const { data: { user } } = await supabase.auth.getUser();
+
+    if (user) {
+        const userId = user.id;
+
+        const updatedUserData = {
+            name: nameInput.value,
+            surname: surnameInput.value,
+            email: emailInput.value,
+            description: descriptionInput.value,
+            instagram_account: instagramInput.value,
+        };
+
+        try {
+            const { data, error } = await supabase
+                .from('users')
+                .update(updatedUserData)
+                .eq('email', user.email);
+
+            if (error) {
+                console.error('Error updating user data:', error.message);
+            } else {
+                console.log('User data updated successfully:', data);
+            }
+        } catch (error) {
+            console.error('Error:', error.message);
+        }
+    }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    const editButton = document.getElementById('edit-button');
+    if (editButton) {
+        editButton.addEventListener('click', updateUserData);
+    }
+});
+
+
--- public/scripts/script.js
+++ public/scripts/script.js
@@ -5,3 +5,11 @@ function goToLogin() {
 function goToSignUp() {
     window.location.href = "signup.html";
 }
+
+function goToProfile() {
+    window.location.href = "userprofileedit.html";
+}
+
+function goToHome() {
+    window.location.href = "home.html";
+}
